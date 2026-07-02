f027671e6456b8bdd0a30b7aca6de059a015e4ac
fariz7172 <fariz7172@gmail.com>
2026-07-02 16:46:54 +0700

    Enlarge signature margins to 4.0cm specifically for pages 5, 6, 7 of sptjm_ls

--- resources/views/payments/print.blade.php
+++ resources/views/payments/print.blade.php
@@ -64,6 +64,9 @@
             .print-area ~ .print-area .mt-28,
             .print-area ~ .print-area .mt-32 { margin-top: 3.0cm !important; }
             
+            /* Jarak sangat besar khusus untuk halaman tertentu yang membutuhkan stempel besar */
+            .print-area .signature-space-large { margin-top: 4.0cm !important; }
+            
             /* STRATEGI TERAKHIR: Hapus SEMUA margin vertikal KHUSUS Halaman 1 agar PASTI muat */
             .page-1-spp .mb-6 { margin-bottom: 0.1cm !important; }
             .page-1-spp .mb-4 { margin-bottom: 0.1cm !important; }
@@ -1109,7 +1112,7 @@
                 <div class="flex flex-col items-center">
                     <p>Jakarta, <span contenteditable="true" data-eid="119">{{ $payment->tgl_spm ? $payment->tgl_spm->translatedFormat('d F Y') : '-' }}</span></p>
                     <p class="uppercase mt-2 text-center">Kepala Suku Dinas Sumber Daya Air<br>Kota Administrasi Jakarta Utara</p>
-                    <div class="mt-24"><p class="underline uppercase text-center" contenteditable="true" data-eid="120">HERIA SUWANDI</p><p class="text-center">NIP. <span contenteditable="true" data-eid="121">197101272006041009</span></p></div>
+                    <div class="mt-24 signature-space-large"><p class="underline uppercase text-center" contenteditable="true" data-eid="120">HERIA SUWANDI</p><p class="text-center">NIP. <span contenteditable="true" data-eid="121">197101272006041009</span></p></div>
                 </div>
             </div>
         </div>
@@ -1208,7 +1211,7 @@
             </div>
             <p class="text-justify leading-relaxed text-[10.5pt] mb-4">Berdasarkan pengajuan Surat Permintaan Pembayaran LS Nomor <span class="">{{ $payment->no_spp }}</span> Tanggal {{ ($payment->tgl_spp ?? $payment->tgl_spm) ? ($payment->tgl_spp ?? $payment->tgl_spm)->translatedFormat('d F Y') : '-' }} telah dilakukan verifikasi terhadap kelengkapan dokumen pendukung sesuai dengan checklist terlampir. Atas surat permintaan pembayaran dan kelengkapan dokumen sebagaimana dimaksud dinyatakan lengkap dan sah sesuai peraturan perundang-undangan untuk dapat diproses sebagai persyaratan Perintah Membayar yang dituangkan dalam surat Surat Perintah Membayar. Jika dikemudian hari pernyataan saya ini tidak benar, maka saya bersedia diberikan sanksi sesuai peraturan yang berlaku.</p>
             <p class="text-justify text-[10.5pt] leading-relaxed mb-12">Demikian Surat ini saya buat dalam keadaan sadar dan tanpa paksaan dari pihak manapun.</p>
-            <div class="flex flex-col items-end mr-4"><div class="text-center min-w-[350px] text-[10.5pt]"><p>Jakarta, <span contenteditable="true" data-eid="138">{{ ($payment->tgl_spp ?? $payment->tgl_spm) ? ($payment->tgl_spp ?? $payment->tgl_spm)->translatedFormat('d F Y') : '-' }}</span></p><p class="mt-1 text-center leading-tight">{{ $payment->pptk->jabatan ?? 'Kepala Seksi Pengolahan Pengendali Banjir, Air Bersih dan Air Limbah' }}<br>Suku Dinas Sumber Daya Air<br>Kota Administrasi Jakarta Utara</p><div class="mt-32"><p class="underline uppercase text-center text-center" contenteditable="true" data-eid="139">{{ $payment->pptk->nama ?? 'YUDO WIDIATMOKO' }}</p><p class="text-center text-center">NIP. <span contenteditable="true" data-eid="140">{{ $payment->pptk->nip ?? '198608302010011010' }}</span></p></div></div></div>
+            <div class="flex flex-col items-end mr-4"><div class="text-center min-w-[350px] text-[10.5pt]"><p>Jakarta, <span contenteditable="true" data-eid="138">{{ ($payment->tgl_spp ?? $payment->tgl_spm) ? ($payment->tgl_spp ?? $payment->tgl_spm)->translatedFormat('d F Y') : '-' }}</span></p><p class="mt-1 text-center leading-tight">{{ $payment->pptk->jabatan ?? 'Kepala Seksi Pengolahan Pengendali Banjir, Air Bersih dan Air Limbah' }}<br>Suku Dinas Sumber Daya Air<br>Kota Administrasi Jakarta Utara</p><div class="mt-32 signature-space-large"><p class="underline uppercase text-center text-center" contenteditable="true" data-eid="139">{{ $payment->pptk->nama ?? 'YUDO WIDIATMOKO' }}</p><p class="text-center text-center">NIP. <span contenteditable="true" data-eid="140">{{ $payment->pptk->nip ?? '198608302010011010' }}</span></p></div></div></div>
         </div>
         @endif
 
@@ -1264,11 +1267,11 @@
             <div class="grid grid-cols-2 text-center gap-4 px-4 text-[10pt] leading-tight mt-8 items-start">
                 <div class="flex flex-col items-center h-full justify-between">
                     <p class="uppercase mt-2 text-center">Pejabat Pelaksana Teknis Kegiatan</p>
-                    <div class="mt-24"><p class="underline uppercase text-center" contenteditable="true" data-eid="152">{{ $payment->pptk->nama ?? 'YUDO WIDIATMOKO' }}</p><p class="text-center">NIP. <span contenteditable="true" data-eid="153">{{ $payment->pptk->nip ?? '198608302010011010' }}</span></p></div>
+                    <div class="mt-24 signature-space-large"><p class="underline uppercase text-center" contenteditable="true" data-eid="152">{{ $payment->pptk->nama ?? 'YUDO WIDIATMOKO' }}</p><p class="text-center">NIP. <span contenteditable="true" data-eid="153">{{ $payment->pptk->nip ?? '198608302010011010' }}</span></p></div>
                 </div>
                 <div class="flex flex-col items-center h-full justify-between">
                     <p class="uppercase mt-2 text-center">Kepala Suku Dinas Sumber Daya Air<br>Kota Administrasi Jakarta Utara</p>
-                    <div class="mt-24"><p class="underline uppercase text-center" contenteditable="true" data-eid="120">HERIA SUWANDI</p><p class="text-center">NIP. <span contenteditable="true" data-eid="121">197101272006041009</span></p></div>
+                    <div class="mt-24 signature-space-large"><p class="underline uppercase text-center" contenteditable="true" data-eid="120">HERIA SUWANDI</p><p class="text-center">NIP. <span contenteditable="true" data-eid="121">197101272006041009</span></p></div>
                 </div>
             </div>
         </div>
